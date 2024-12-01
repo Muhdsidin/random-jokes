37fb33617e3ab6f6bfcc5f83f885914ab36c0dda
Logic-leap <logicleap00@gmail.com>
2024-12-01 20:53:42 +0530

    first commit for just pushing

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <> 
+     <button className="bg-slate-500 hover:bg-slate-600 text-white font-bold py-2 px-4 rounded" >Generate Joke </button>
+
+    </>
+  )
+}
+
+export default Home
